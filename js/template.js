33202abe188072a9daa01d3d3369eee0290b7283
Vitaly Penchuk <vitaly.penchuk@gmail.com>
2017-09-29 12:30:42 +0300

    trello app

--- js/template.js
+++ js/template.js
@@ -2,16 +2,16 @@
 var GRAY_ICON = './images/icon-gray.svg';
 var MP_ICON = './images/timer.png';
 
-var tokenLooksValid = function(token) {
+var tokenLooksValid = function (token) {
     return /^[0-9a-f]{64}$/.test(token);
 };
 
 var isTrelloAuthorized = function () {
-    if(!localStorage.getItem('token')){
+    if (!localStorage.getItem('token')) {
         var t = window.TrelloPowerUp.iframe();
         var oauthUrl = 'https://trello.com/1/authorize?expiration=never' +
-            '&name=Trello%20for%20Chrome&scope=read,write,account&key='+Trello.key()+'&callback_method=fragment' +
-            '&return_url='+encodeURIComponent(window.location.origin+'/auth.html');
+            '&name=Trello%20for%20Chrome&scope=read,write,account&key=' + Trello.key() + '&callback_method=fragment' +
+            '&return_url=' + encodeURIComponent(window.location.origin + '/auth.html');
 
 
         var authorizeOpts = {
@@ -20,56 +20,61 @@ var isTrelloAuthorized = function () {
             validToken: tokenLooksValid
         };
         t.authorize(oauthUrl, authorizeOpts)
-            .then(function(token) {
+            .then(function (token) {
                 localStorage.setItem('token', token);
                 return true
             })
-            .then(function() {
+            .then(function () {
                 return t.closePopup();
             });
         return false;
-    }else{
+    } else {
         return true;
     }
 };
 
 
+var cardButtonCallback = function (t) {
+    if (isMPAutorized()) {
 
-var cardButtonCallback = function(t){
-    if(localStorage.getItem('mp_token')){
-
-    }else{
+    } else {
         return openLoginModal(t);
     }
 };
 
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
-    if(isMPAutorized()){
-        _mpAjax({
-            method: 'GET',
-            url: 'timer/status',
-            params: {},
-            success: function (data) {
-            },
-            error: function (data) {
-                if(data.status == 401){
-                    console.log(t);
-                };
-            }
-        })
+    'card-buttons': function (t, options) {
+        if (isMPAutorized()) {
+            _mpAjax({
+                method: 'GET',
+                url: 'timer/status',
+                params: {},
+                success: function (data) {
+                    return [{
+                        icon: MP_ICON,
+                        text: 'Start timer 123',
+                        callback: cardButtonCallback
+                    }];
+                },
+                error: function (data) {
+                    if (data.status == 401) {
+                        localStorage.removeItem('mp_token');
+                    }
+                }
+            })
+        }else{
+            return [{
+                icon: MP_ICON,
+                text: 'Start timer',
+                callback: cardButtonCallback
+            }];
+        }
+    },
+    'show-settings': function (t, options) {
+        return t.popup({
+            title: 'Settings',
+            url: './settings.html',
+            height: 184
+        });
     }
-      return [{
-          icon: MP_ICON,
-          text: 'Start timer',
-          callback: cardButtonCallback
-      }];
-  },
-  'show-settings': function(t, options){
-    return t.popup({
-      title: 'Settings',
-      url: './settings.html',
-      height: 184
-    });
-  }
 });
