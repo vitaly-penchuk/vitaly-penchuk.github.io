137ca5a35230332bfb6c392cf895a8402c5cc7da
Vitaly Penchuk <vitaly.penchuk@gmail.com>
2017-09-29 17:00:22 +0300

    trello app

--- js/timer.js
+++ js/timer.js
@@ -2,27 +2,29 @@ $(function () {
     var t = window.TrelloPowerUp.iframe();
     t.sizeTo('.container');
 
-    _mpAjax({
-        method: 'GET',
-        url: 'timer/projects',
-        params: {},
-        success: function (response) {
-            var data = response.data;
-            console.log(data);
-        }
-    });
-
-    _mpAjax({
-        method: 'GET',
-        url: 'timer/status',
-        params: {},
-        success: function (response) {
-            var data = response.data;
-        },
-        error: function (data) {
-            if (data.status == 401) {
-                localStorage.removeItem('mp_token');
+    if(isMPAutorized()){
+        _mpAjax({
+            method: 'GET',
+            url: 'timer/projects',
+            params: {},
+            success: function (response) {
+                var data = response.data;
+                console.log(data);
             }
-        }
-    });
+        });
+
+        _mpAjax({
+            method: 'GET',
+            url: 'timer/status',
+            params: {},
+            success: function (response) {
+                var data = response.data;
+            },
+            error: function (data) {
+                if (data.status == 401) {
+                    localStorage.removeItem('mp_token');
+                }
+            }
+        });
+    }
 });
