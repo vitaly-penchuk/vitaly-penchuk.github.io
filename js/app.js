797057c32c139941e36630128d159da6593ac3c0
Vitaly Penchuk <vitaly.penchuk@gmail.com>
2017-09-29 10:00:12 +0300

    trello app

--- js/app.js
+++ js/app.js
@@ -25,10 +25,43 @@ var mpAjax = function (options) {
 
 var openLoginModal = function (t) {
     return t.modal({
-        title: 'Timer',
-        url: './timer.html',
-        height: 500,
+        title: 'MoneyPenny Login',
+        url: './login.html',
+        height: 400,
         fullscreen: false,
         accentColor: '#e27702'
     });
 };
+
+$(function(){
+    var options = {
+        error : function (data) {
+            $('#login-error').html(data.message);
+        }
+    };
+    $('#login-btn').on('click',function (e) {
+        e.preventDefault();
+        var node = $(this);
+        var form = node.closest('form');
+        var params = form.serialize();
+        $.ajax({
+            url: 'https://api.moneypenny.me/'+form.find('#tenant_name').val()+'/login',
+            data: params,
+            method: 'POST',
+            error: function (jqXHR, status, error) {
+                var response = jqXHR.responseJSON;
+                options.error(response.data);
+            },
+            success: function (data, status, jqXHR) {
+                var data = data.data;
+                var mp_token = {
+                    tenant: $('#tenant_name').val(),
+                    token: data.access_token
+                };
+                localStorage.setItem('mp_token', JSON.stringify(mp_token));
+                var t = window.TrelloPowerUp.iframe();
+                t.closeModal();
+            }
+        });
+    });
+});
