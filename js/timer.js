b006b3068982c7e97fc4f45067f039067fc2d3d9
Vitaly Penchuk <vitaly.penchuk@gmail.com>
2017-10-03 14:51:13 +0300

    trello app

--- js/timer.js
+++ js/timer.js
@@ -1,10 +1,51 @@
+var project_dd = $('#project_id');
+
+var task_dd = $('#task_id');
+
+var populateProjects = function (project_id,task_id, callback) {
+    _mpAjax({
+        method: 'GET',
+        url: 'timer/projects',
+        params: {},
+        success: function (response) {
+            var data = response.data;
+            var optgroups = {};
+            project_dd.html('').val('');
+            project_dd.append('<option value=""></option>');
+            $.each(data,function (index, project) {
+                if(!optgroups.hasOwnProperty(project.client_name)){
+                    optgroups[project.client_name] = $('<optgroup label="'+project.client_name+'">');
+                    project_dd.append(optgroups[project.client_name]);
+                }
+                optgroups[project.client_name].append('<option value="'+project.id+'">'+project.name+'</option>')
+            });
+            project_dd.val(project_id);
+            if (typeof callback == "function" && task_id){
+                callback(project_id,task_id);
+            }
+        }
+    });
+};
+
+var populateTasks = function (project_id,task_id) {
+    _mpAjax({
+        method: 'GET',
+        url: 'timer/project/'+project_id+'/tasks',
+        params: {},
+        success: function (response) {
+            var data = response.data;
+            task_dd.html('').val('');
+            task_dd.append('<option value=""></option>');
+            $.each(data,function (index, task) {
+                task_dd.append('<option value="'+task.id+'">'+task.name+'</option>')
+            });
+            task_dd.val(task_id);
+        }
+    });
+};
 $(function () {
     var t = window.TrelloPowerUp.iframe();
     t.sizeTo('.container');
-
-    var project_dd = $('#project_id');
-
-    var task_dd = $('#task_id');
 
     project_dd.on('change',function (e) {
        var node = $(this);
@@ -14,49 +55,7 @@ $(function () {
        }else{
            task_dd.html('').val('');
        }
-    });
-
-    var populateProjects = function (project_id,task_id, callback) {
-        _mpAjax({
-            method: 'GET',
-            url: 'timer/projects',
-            params: {},
-            success: function (response) {
-                var data = response.data;
-                var optgroups = {};
-                project_dd.html('').val('');
-                project_dd.append('<option value=""></option>');
-                $.each(data,function (index, project) {
-                    if(!optgroups.hasOwnProperty(project.client_name)){
-                        optgroups[project.client_name] = $('<optgroup label="'+project.client_name+'">');
-                        project_dd.append(optgroups[project.client_name]);
-                    }
-                    optgroups[project.client_name].append('<option value="'+project.id+'">'+project.name+'</option>')
-                });
-                project_dd.val(project_id);
-                if (typeof callback == "function" && task_id){
-                    callback(project_id,task_id);
-                }
-            }
-        });
-    };
-
-    var populateTasks = function (project_id,task_id) {
-        _mpAjax({
-            method: 'GET',
-            url: 'timer/project/'+project_id+'/tasks',
-            params: {},
-            success: function (response) {
-                var data = response.data;
-                task_dd.html('').val('');
-                task_dd.append('<option value=""></option>');
-                $.each(data,function (index, task) {
-                    task_dd.append('<option value="'+task.id+'">'+task.name+'</option>')
-                });
-                task_dd.val(task_id);
-            }
-        });
-    };
+    })
 
     if(isMPAutorized()){
         getMPTimerStatus(populateProjects);
